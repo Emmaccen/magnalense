b67214dce4c4428259dde936ccf454e265bac6e2
chubbi stephen <udeokoliestephen@gmail.com>
2023-11-19 01:39:28 +0100

    Worked on typography and theming.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { Inter, Work_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-inter',
+});
 
-export const metadata: Metadata = {
-	title: 'Magnalense',
-	description: 'Where care meets sight',
-};
+const roboto_mono = Work_Sans({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-work-sans',
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
+		<html lang="en" className={`${inter.variable} work_sans`}>
+			<body>{children}</body>
 		</html>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,24 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		colors: {
+			primary: '#ed7d31',
+			neutral: '#272829',
+			pink: '#ff49db',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-inter)'],
+				mono: ['var(--font-roboto-mono)'],
+			},
+		},
+	},
+	plugins: [],
 };
-export default config;
