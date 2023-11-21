85c218a52f216aa1489293430110609dfa30b059
Emmanuel Lucius <58217705+Emmaccen@users.noreply.github.com>
2023-11-21 23:06:13 +0100

    Font init and theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
-import { Inter, Work_Sans } from 'next/font/google';
-
-const inter = Inter({
-	subsets: ['latin'],
-	display: 'swap',
-	variable: '--font-inter',
-});
-
-const roboto_mono = Work_Sans({
-	subsets: ['latin'],
-	display: 'swap',
-	variable: '--font-work-sans',
-});
+import { inter } from './font';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" className={`${inter.variable} work_sans`}>
+		<html lang="en" className={`${inter.className}`}>
 			<body>{children}</body>
 		</html>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
+			// max-width
+
+			'-2xl': { max: '1535px' },
+			// => @media (max-width: 1535px) { ... }
+
+			'-xl': { max: '1279px' },
+			// => @media (max-width: 1279px) { ... }
+
+			'-lg': { max: '1023px' },
+			// => @media (max-width: 1023px) { ... }
+
+			'-md': { max: '767px' },
+			// => @media (max-width: 767px) { ... }
+
+			'-sm': { max: '639px' },
+			// => @media (max-width: 639px) { ... }
+			'-xs': { max: '550px' },
+
+			// min-width
+			xs: '551px',
+			// => @media (min-width: 551px) { ... }
+			xxs: { min: '470px' },
+
+			...defaultTheme.screens,
 		},
 		colors: {
 			primary: '#ed7d31',
@@ -15,8 +38,7 @@ module.exports = {
 		},
 		extend: {
 			fontFamily: {
-				sans: ['var(--font-inter)'],
-				mono: ['var(--font-roboto-mono)'],
+				fontInter: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
